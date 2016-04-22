c1ae8c8172982a033291f26f768f2e5af1bc080e
Judd Niemann <jniemann666@bigpond.com>
2016-04-22 20:08:04 +1000

    improvements to dithering

---
+++ biquad.h
@@ -0,0 +1,38 @@
+#ifndef biquad_H
+#define biquad_H
+
+template<typename FloatType> class Biquad {
+public:
+	
+	// Naming conventions for coefficients are as follows:
+	// numerator/input coeffs:			a0,a1,a2
+	// denominator/feedback coeffs:		b1,b2
+	Biquad() {};
+	Biquad(FloatType a0, FloatType a1, FloatType a2, FloatType b1, FloatType b2)
+		: a0(a0), a1(a1), a2(a2), b1(b1), b2(b2),z1(0.0),z2(0.0) {};
+
+	FloatType filter(FloatType inSample) {
+
+		// Biquad calculation using transposed Direct Form 2:
+		
+		FloatType outSample = inSample * a0 + z1;
+		z1 = inSample * a1 + z2 - b1 * outSample	;
+		z2 = inSample * a2 - b2 * outSample;
+		return outSample;
+	}
+
+	void setCoeffs(FloatType a0, FloatType a1, FloatType a2, FloatType b1, FloatType b2) {
+		Biquad::a0 = a0;
+		Biquad::a1 = a1;
+		Biquad::a2 = a2;
+		Biquad::b1 = b1;
+		Biquad::b2 = b2;
+	}
+
+    
+protected:
+    FloatType a0, a1, a2, b1, b2;
+    double z1, z2;
+};
+
+#endif biquad_H
--- ditherer.h
+++ ditherer.h
@@ -9,9 +9,16 @@
 #ifndef DITHERER_H
 #define DITHERER_H 1
 
-#include <cmath>
+ #define USE_IIR // if defined, use IIR Filter for noise shaping, otherwise use FIR. 
+// Note: through experimentation, it has proven difficult to get the desired response curve using FIR
+// that is why IIR was substituted ...
 
-#define NOISE_SHAPING_FILTER_SIZE 11
+#include <cmath>
+#include "biquad.h"
+
+#ifndef USE_IIR
+	#define NOISE_SHAPING_FILTER_SIZE 11
+#endif // !USE_IIR
 
 template<typename FloatType>
 class Ditherer {
@@ -19,49 +26,91 @@ public:
 	unsigned int signalBits;
 	FloatType ditherBits;
 	
-	Ditherer(unsigned int signalBits, FloatType ditherBits) 
-		: signalBits(signalBits), ditherBits(ditherBits)
+	Ditherer(unsigned int signalBits, FloatType ditherBits)
+		: signalBits(signalBits), ditherBits(ditherBits), E(0)
+		
 	{
-		//
-		signalMagnitude = static_cast<FloatType>(1 << (signalBits - 1));
-		ditherMagnitude = pow(2,ditherBits-1) / signalMagnitude / RAND_MAX;
-		oldRandom = newRandom = 0;
+
+#ifdef USE_IIR
+		// IIR-related stuff:
+		if (ditherBits < 1.5)
+		{
+			// IIR noise-shaping filter (2 biquads) - flatter response; more energy in spectrum
+			f1.setCoeffs(0.798141839881378, -0.7040563852194521, 0.15341541599754416, 0.3060312586301247, 0.02511886431509577);
+			f2.setCoeffs(0.5, -0.7215722413008345, 0.23235922079486643, -1.5531272249269004, 0.7943282347242815);
+		}
+		else
+		{	
+			// IIR noise-shaping filter (2 biquads)
+			f1.setCoeffs(0.1872346691747817, -0.1651633303505913, 0.03598944852318585, 1.2861600144545022, 0.49000000000000016);
+			f2.setCoeffs(0.5, -0.7215722413008345, 0.23235922079486643, -1.2511963408503206, 0.5328999999999999);
+		}
+#else 
+		// FIR-related stuff:
 		currentIndex = NOISE_SHAPING_FILTER_SIZE - 1;
 		memset(noise, 0, NOISE_SHAPING_FILTER_SIZE * sizeof(FloatType));
+#endif // USE_IIR
+		
+		signalMagnitude = static_cast<FloatType>(1 << (signalBits - 1));
+		reciprocalSignalMagnitude = 1.0 / signalMagnitude;
+		ditherMagnitude = pow(2,ditherBits-1) / signalMagnitude / RAND_MAX;
+		oldRandom = newRandom = 0;
+		
 	}
 	
 	FloatType Dither(FloatType inSample) {
-		newRandom = rand();
-		
-		// put tpdf noise into history buffer (noise goes in "backwards"):
-		noise[currentIndex] = ditherMagnitude * static_cast<FloatType>(newRandom - oldRandom);
-		currentIndex = currentIndex ? currentIndex-1 : NOISE_SHAPING_FILTER_SIZE - 1; 
-	
-		// Filter the noise:
 		FloatType shapedNoise = 0.0;
+		newRandom = rand(); // to-do: better quality randoms ?
+		inSample -= E; // apply Error Feedback
+		FloatType tpdfNoise = ditherMagnitude * static_cast<FloatType>(newRandom - oldRandom);
+
+#ifndef USE_IIR
+		// FIR Noise Shaping:
+		// put tpdf noise into history buffer (noise goes in "backwards"):
+		noise[currentIndex] = tpdfNoise;
+		currentIndex = currentIndex ? currentIndex - 1 : NOISE_SHAPING_FILTER_SIZE - 1;
+
+		// Filter the noise:
+
 		int index = currentIndex;
-		for (int i = 0; i <  NOISE_SHAPING_FILTER_SIZE; ++i) {
+		for (int i = 0; i < NOISE_SHAPING_FILTER_SIZE; ++i) {
 			shapedNoise += noise[index] * NoiseShapeCoeffs[i];
 			index = (index == NOISE_SHAPING_FILTER_SIZE - 1) ? 0 : index + 1;
 		}
+#else
+		// IIR Noise Shaping:
+		shapedNoise = 1.05 * // tweak factor
+			f2.filter(f1.filter(tpdfNoise)); // filter the triangular noise with two cascaded biQuads 
+#endif
 
-		oldRandom = newRandom;
 		outSample = inSample + shapedNoise;
 		
+		// Calculate the quantization error. This needs to exactly model the behavior of the I/O library writing samples to outfile, 
+		// which in our case, appears to use round() ( ... as opposed to floor(), or cast-to-integer ...)
+		
+		quantizedOutSample = round(signalMagnitude * outSample); // quantize
+		E = reciprocalSignalMagnitude * quantizedOutSample - inSample; // calculate error 
+		
+		oldRandom = newRandom;
 		return outSample;
 	}
 
 private:
 	int oldRandom, newRandom;
-		
+	FloatType E;				// last Quantization error
+	int quantizedOutSample;
 	FloatType outSample;
-	FloatType signalMagnitude;
-	FloatType ditherMagnitude;
+	FloatType signalMagnitude;	// maximum integral value for signal target bit depth (for quantizing) 
+	FloatType reciprocalSignalMagnitude; // for normalizing quantized signal back to +/- 1.0 
+	FloatType ditherMagnitude;	// maximum integral value for dither target bit depth
+	
+#ifdef USE_IIR
+	// IIR Filters:
+	Biquad<double> f1;
+	Biquad<double> f2;
+#else	
+	// FIR Filter-related stuff:
 	int currentIndex;
-	
-	// to-do: feed quantize error signal into filter
-	// to-do: FIR doesn't really cut it; need IIR to get the right curve !!
-	
 	// small FIR filter for noise-shaping:
 	double NoiseShapeCoeffs[NOISE_SHAPING_FILTER_SIZE] = {
 	
@@ -77,6 +126,19 @@ private:
 		0.06696663418581869,
 		-0.0010319367876351854,
 		-0.014702063883960252
+
+		// super-duper 9-tap (Psychoacoustically Optimal Noise Shaping):
+		/*2.412,
+		-3.370,
+		3.937,
+		-4.174,
+		3.353,
+		-2.205,
+		1.281,
+		-0.569,
+		0.0847*/
+
+
 
 		// previous filter attempts: 
 
@@ -102,8 +164,9 @@ private:
 	
 	};
 
-	// (circular) buffer for tpdf noise history:
+	// (circular) buffer for noise history:
 	FloatType noise[NOISE_SHAPING_FILTER_SIZE];
+#endif
 };
 
 #endif // !DITHERER_H
