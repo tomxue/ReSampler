8d7db113a6f6a4a868ccaf4e411b78e72b944e73
Judd Niemann <jniemann666@bigpond.com>
2017-01-15 02:28:37 +1100

    refactored

--- FIRFilterAVX.h
+++ FIRFilterAVX.h
@@ -1,19 +1,20 @@
 #ifndef FIRFFILTER_AVX_H_
 #define FIRFFILTER_AVX_H_
-#ifdef USE_AVX
-
-#define AVX_ALIGNMENT_SIZE 32
-
-//#define USE_FMA 1
-
-// FIRFilterAVX.h : AVX-specific code for FIR filtering
 
 #include <immintrin.h>
 #include <typeinfo>
 #include <cstdint>
 #include <cassert>
-
 #include "alignedmalloc.h"
+
+#ifdef USE_AVX
+#define AVX_ALIGNMENT_SIZE 32
+static inline float sum8floats(__m256 x);
+static inline double sum4doubles(__m256d x);
+
+//#define USE_FMA 1
+
+// FIRFilterAVX.h : AVX-specific code for FIR filtering
 
 template <typename FloatType>
 class FIRFilter {
@@ -21,7 +22,7 @@ class FIRFilter {
 public:
 	// constructor:
 	FIRFilter(const FloatType* taps, size_t size) :
-		size(size), sizeRounded8((size >> 3) << 3), CurrentIndex(size-1), LastPut(0),
+		size(size), sizeRounded8((size >> 3) << 3), sizeRounded4((size >> 2) << 2), CurrentIndex(size-1), LastPut(0),
 		Signal(NULL), Kernel0(NULL), Kernel1(NULL), Kernel2(NULL), Kernel3(NULL), Kernel4(NULL), Kernel5(NULL), Kernel6(NULL), Kernel7(NULL)
 	{
 		// allocate buffers:
@@ -60,7 +61,7 @@ public:
 	}
 
 	// copy constructor: 
-	FIRFilter(const FIRFilter& other) : size(other.size), sizeRounded8(other.sizeRounded8), CurrentIndex(other.CurrentIndex), LastPut(other.LastPut)
+	FIRFilter(const FIRFilter& other) : size(other.size), sizeRounded8(other.sizeRounded8), sizeRounded4(other.sizeRounded4), CurrentIndex(other.CurrentIndex), LastPut(other.LastPut)
 	{
 		allocateBuffers();
 		assertAlignment();
@@ -69,7 +70,7 @@ public:
 
 	// move constructor:
 	FIRFilter(FIRFilter&& other) :
-		size(other.size), sizeRounded8(other.sizeRounded8), CurrentIndex(other.CurrentIndex), LastPut(other.LastPut),
+		size(other.size), sizeRounded8(other.sizeRounded8), sizeRounded4(other.sizeRounded4), CurrentIndex(other.CurrentIndex), LastPut(other.LastPut),
 		Signal(other.Signal), Kernel0(other.Kernel0), Kernel1(other.Kernel1), Kernel2(other.Kernel2), Kernel3(other.Kernel3),
 		Kernel4(other.Kernel4), Kernel5(other.Kernel5), Kernel6(other.Kernel6), Kernel7(other.Kernel7)
 	{
@@ -90,6 +91,7 @@ public:
 	{
 		size = other.size;
 		sizeRounded8 = other.sizeRounded8;
+		sizeRounded4 = other.sizeRounded4;
 		CurrentIndex = other.CurrentIndex;
 		LastPut = other.LastPut;
 		freeBuffers();
@@ -106,6 +108,7 @@ public:
 		{
 			size = other.size;
 			sizeRounded8 = other.sizeRounded8;
+			sizeRounded4 = other.sizeRounded4;
 			CurrentIndex = other.CurrentIndex;
 			LastPut = other.LastPut;
 
@@ -234,7 +237,6 @@ public:
 		alignas(AVX_ALIGNMENT_SIZE) __m256 product;
 		alignas(AVX_ALIGNMENT_SIZE) __m256 accumulator = _mm256_setzero_ps();
 
-	//	for (int i = 8; i < (size >> 3) << 3; i += 8) {
 		for (int i = 8; i < sizeRounded8; i += 8) {
 			signal = _mm256_load_ps(Signal + Index);
 			kernel = _mm256_load_ps(Kernel + i);
@@ -262,7 +264,6 @@ public:
 		output += sum8floats(accumulator);
 
 		// Part 3: Tail
-		//for (int j = (size >> 3) << 3; j < size; ++j) {
 		for (int j = sizeRounded8; j < size; ++j) {
 			output += Signal[Index] * Kernel[j];
 			++Index;
@@ -287,6 +288,7 @@ public:
 private:
 	size_t size;
 	size_t sizeRounded8; // size rounded down to nearest multiple of 8
+	size_t sizeRounded4; // size rounded down to nearest multiple of 4
 	FloatType* Signal; // Double-length signal buffer, to facilitate fast emulation of a circular buffer
 	int CurrentIndex;
 	int LastPut;
@@ -425,42 +427,52 @@ double FIRFilter<double>::get() {
 	alignas(AVX_ALIGNMENT_SIZE) __m256d product;
 	alignas(AVX_ALIGNMENT_SIZE) __m256d accumulator = _mm256_setzero_pd();
 
-	for (int i = 4; i < (size >> 2) << 2; i += 4) {
+	for (int i = 4; i < sizeRounded4; i += 4) {
 		signal = _mm256_load_pd(Signal + Index);
 		kernel = _mm256_load_pd(Kernel + i);
 		product = _mm256_mul_pd(signal, kernel);
 		accumulator = _mm256_add_pd(product, accumulator);
 		Index += 4;
 	}
-
+	/*
 	output +=
 		accumulator.m256d_f64[0] +
 		accumulator.m256d_f64[1] +
 		accumulator.m256d_f64[2] +
 		accumulator.m256d_f64[3];
+	*/
+	output += sum4doubles(accumulator);
 
 	// Part 3: Tail
-	for (int j = (size >> 2) << 2; j < size; ++j) {
+	for (int j = sizeRounded4; j < size; ++j) {
 		output += Signal[Index] * Kernel[j];
 		++Index;
 	}
-
 	return output;
 }
 
 // Horizontal add function (sums 8 floats into single float) http://stackoverflow.com/questions/23189488/horizontal-sum-of-32-bit-floats-in-256-bit-avx-vector
 static inline float sum8floats(__m256 x) {
-	const __m128 x128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));		// ( x3+x7, x2+x6, x1+x5, x0+x4 )
+	const __m128 x128 = _mm_add_ps(
+		_mm256_extractf128_ps(x, 1),
+		_mm256_castps256_ps128(x));																// ( x3+x7, x2+x6, x1+x5, x0+x4 )
 	const __m128 x64 = _mm_add_ps(x128, _mm_movehl_ps(x128, x128));								// ( -, -, x1+x3+x5+x7, x0+x2+x4+x6 )
 	const __m128 x32 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));							// ( -, -, -, x0+x1+x2+x3+x4+x5+x6+x7 )
 	return _mm_cvtss_f32(x32);
 }
 
-// Horizontal add function (sums 4 doubles into single double) - unfinished
-static inline double sum4doubles(__m256 x) {
-	// to-do: finish this ...
-	const __m128d x64;
+// Horizontal add function (sums 4 doubles into single double)
+static inline double sum4doubles(__m256d x) {
+	const __m128d x128 = _mm_add_pd(
+		_mm256_extractf128_pd(x, 1),
+		_mm256_castpd256_pd128(x));
+	const __m128d x64 = _mm_add_pd(_mm_permute_pd(x128, 1), x128);
 	return _mm_cvtsd_f64(x64);
+}
+
+double testSum4Doubles(double a, double b, double c, double d) {
+	const __m256d v = _mm256_set_pd(a, b, c, d);
+	return sum4doubles(v);
 }
 
 
