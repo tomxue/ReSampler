ab80206c686a9a184ac9a8aba436b6d98b18a85c
Judd Niemann <jniemann666@bigpond.com>
2017-03-07 11:34:55 +1100

    timings: <chrono> api instead of QueryPerformanceCounter()

--- osspecific.h
+++ osspecific.h
@@ -1,14 +1,22 @@
 #ifndef OSSPECIFIC_H
 #define OSSPECIFIC_H 1
 
-// macros which address differences between operating systems go here
+// macros which address differences between operating systems go here.
+
+#define USE_CHRONO // use the std <chrono> library for timing.  
 
 #ifdef _WIN32
-#define NOMINMAX // don't use min() max() macros; use std:: library instead
-#include <windows.h>
+#define NOMINMAX // disable min() and max() macros (use std:: library instead)
+
 #pragma warning(disable : 4996) // suppress pointless MS "deprecation" warnings
 #pragma warning(disable : 4244) // suppress double-to-float warnings
+
 // Timer macros:
+#ifndef USE_CHRONO
+// don't use the C++11 <chrono> api ...
+// Background: Older versions of MSVC prior to 2015 had issues with low timer resolution.
+
+#include <windows.h>
 #define START_TIMER() LARGE_INTEGER starttime,finishtime,elapsed,frequency,timetaken; \
 	QueryPerformanceFrequency(&frequency); \
 	QueryPerformanceCounter(&starttime)
@@ -17,15 +25,21 @@
 	elapsed.QuadPart=finishtime.QuadPart-starttime.QuadPart; \
 	timetaken.QuadPart=((1000*elapsed.QuadPart)/frequency.QuadPart); \
 	std::cout << "Time=" << static_cast<long>(timetaken.QuadPart) << " ms" << std::endl
+#else
+#include <chrono>
+#define START_TIMER() auto beginTimer = std::chrono::high_resolution_clock::now()
+#define STOP_TIMER() auto endTimer = std::chrono::high_resolution_clock::now(); \
+std::cout << "Time=" << std::chrono::duration_cast<std::chrono::milliseconds>(endTimer - beginTimer).count() << " ms" << std::endl
+#endif // !USE_CHRONO
 
 #else // Non-Windows:
-
 typedef uint64_t __int64;
 #define stricmp strcasecmp
 // Timer macros:
-#define START_TIMER() std::cout << "(start timer)" << std::endl
-#define STOP_TIMER()  std::cout << "(end timer)" << std::endl
-
+#include <chrono>
+#define START_TIMER() auto beginTimer = std::chrono::high_resolution_clock::now()
+#define STOP_TIMER() auto endTimer = std::chrono::high_resolution_clock::now(); \
+std::cout << "Time=" << std::chrono::duration_cast<std::chrono::milliseconds>(endTimer - beginTimer).count() << " ms" << std::endl
 #endif // ends Non-Windows
 
 #endif // !OSSPECIFIC_H
