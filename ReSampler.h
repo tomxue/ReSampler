08a8a6c440f9e460d968fff6af9fe97719162b3d
Judd Niemann <jniemann666@bigpond.com>
2017-10-22 17:51:41 +1100

    change version designation (to 'pre-release') and rebuild binaries

--- ReSampler.h
+++ ReSampler.h
@@ -18,7 +18,7 @@
 
 struct ConversionInfo;
 
-const std::string strVersion("2.0.0");
+const std::string strVersion("2.0.0-pre-release");
 const std::string strUsage("usage: ReSampler -i <inputfile> [-o <outputfile>] -r <samplerate> [-b <bitformat>] [-n [<normalization factor>]]\n");
 const std::string strExtraOptions(
 	"--help\n"
