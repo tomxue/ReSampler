3143a5f6673ea0effe1a1b93281bc9033554c573
Judd Niemann <jniemann666@bigpond.com>
2017-10-22 17:29:43 +1100

    further clean-ups and tweaks

--- tests/analyze44k-96k.sh
+++ tests/analyze44k-96k.sh
@@ -26,10 +26,6 @@ rm $output_path/._*
 
 $resampler_path -i ./inputs/44khz_sweep-3dBFS_32f.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-final.wav -r 96000 --doubleprecision --showstages
 
-#$resampler_path -i ./inputs/44khz_sweep-3dBFS_32f.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage1.wav -r 73500 --doubleprecision --lpf-cutoff 90.909091 --lpf-transition 5.454545 --maxStages 1
-#$resampler_path -i ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage1.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage2.wav -r 84000 --doubleprecision --lpf-cutoff 90.909091 --lpf-transition 74.772727 --maxStages 1
-#$resampler_path -i ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage2.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp.wav -r 96000 --doubleprecision --lpf-cutoff 90.909091 --lpf-transition 45.738636 --maxStages 1
-
 $resampler_path -i ./inputs/44khz_sweep-3dBFS_32f.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage1.wav -r 73500 --doubleprecision --lpf-cutoff 90.909091 --lpf-transition 5.454545 --maxStages 1
-$resampler_path -i ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage1.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage2.wav -r 84000 --doubleprecision --lpf-cutoff 54.5442 --lpf-transition 74.772727 --maxStages 1
-$resampler_path -i ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage2.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp.wav -r 96000 --doubleprecision --lpf-cutoff 41.7604 --lpf-transition 45.738636 --maxStages 1
+$resampler_path -i ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage1.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage2.wav -r 84000 --doubleprecision --lpf-cutoff 54.545455 --lpf-transition 74.772727 --maxStages 1
+$resampler_path -i ./outputs/44khz_sweep-3dBFS_32f-to96k-dp-stage2.wav -o ./outputs/44khz_sweep-3dBFS_32f-to96k-dp.wav -r 96000 --doubleprecision --lpf-cutoff 47.727273 --lpf-transition 45.738636 --maxStages 1
