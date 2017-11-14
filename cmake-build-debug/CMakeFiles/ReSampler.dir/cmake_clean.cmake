6cfddbfbb880a7c8761bb138c1481b1e5136e71c
Judd Niemann <jniemann666@bigpond.com>
2017-11-14 23:36:06 +1100

    first attempt to use CMake/CLion on windows (unsuccessful)

---
+++ cmake-build-debug/CMakeFiles/ReSampler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReSampler.dir/ReSampler.cpp.obj"
+  "ReSampler.pdb"
+  "ReSampler.exe"
+  "ReSampler.exe.manifest"
+  "libReSampler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReSampler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
