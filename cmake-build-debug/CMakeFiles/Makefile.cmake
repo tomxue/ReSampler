82a140a99c2f4f5a99463374df71f01d4a23fda9
Judd Niemann <jniemann666@bigpond.com>
2017-11-15 00:07:15 +1100

    hmmfph

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -49,5 +49,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/ReSampler.dir/DependInfo.cmake"
+  "CMakeFiles/ReSampler.exe.dir/DependInfo.cmake"
   )
---
+++ cmake-build-debug/CMakeFiles/ReSampler.exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReSampler.exe.dir/ReSampler.cpp.obj"
+  "ReSampler.exe.pdb"
+  "ReSampler.exe.exe"
+  "ReSampler.exe.exe.manifest"
+  "libReSampler.exe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReSampler.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
