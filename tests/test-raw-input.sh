691309b4b5a30f201b5324a9541fabe5342eed19
jniemann66 <jniemann666@bigpond.com>
2019-07-11 19:44:46 +1000

    working towards enabling raw input.

--- tests/test-raw-input.sh
+++ tests/test-raw-input.sh
@@ -37,6 +37,8 @@ $resampler_path -i $input_path/96khz_sweep-3dBFS_32f.wav -o $output_path/96khz_s
 $resampler_path -i $input_path/96khz_sweep-3dBFS_32f.wav -o $output_path/96khz_sweep-3dBFS_32f-to44kdwvw12.raw -r 44100 -b dwvw12
 $resampler_path -i $input_path/96khz_sweep-3dBFS_32f.wav -o $output_path/96khz_sweep-3dBFS_32f-to44kdwvw16.raw -r 44100 -b dwvw16
 $resampler_path -i $input_path/96khz_sweep-3dBFS_32f.wav -o $output_path/96khz_sweep-3dBFS_32f-to44kdwvw24.raw -r 44100 -b dwvw24
+# stereo file to raw pcm
+$resampler_path -i $input_path/guitar.flac -o $output_path/guitar.raw -r 44100 -b 16
 
 # convert raw pcm files back to wav 
 $resampler_path -o $output_path/96khz_sweep-3dBFS_44k8.wav -i $output_path/96khz_sweep-3dBFS_32f-to44k8.raw -r 44100 -b u8 --raw-input 44100 u8
@@ -47,3 +49,5 @@ $resampler_path -o $output_path/96khz_sweep-3dBFS_44k32f.wav -i $output_path/96k
 $resampler_path -o $output_path/96khz_sweep-3dBFS_44k64f.wav -i $output_path/96khz_sweep-3dBFS_32f-to44k64f.raw -r 44100 -b 64f --raw-input 44100 64f
 $resampler_path -o $output_path/96khz_sweep-3dBFS_44kalaw.wav -i $output_path/96khz_sweep-3dBFS_32f-to44kalaw.raw -r 44100 -b alaw --raw-input 44100 alaw
 $resampler_path -o $output_path/96khz_sweep-3dBFS_44kulaw.wav -i $output_path/96khz_sweep-3dBFS_32f-to44kulaw.raw -r 44100 -b ulaw --raw-input 44100 ulaw
+# raw stereo file to wav
+$resampler_path -o $output_path/guitar.wav -i $output_path/guitar.raw -r 44100 -b 16 --raw-input 44100 16 2
