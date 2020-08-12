f2771bff41e8dd1463588a9b4d4d2c766f574619
Judd Niemann <jniemann666@bigpond.com>
2020-08-12 21:44:15 +1000

    testing various differentiators again

--- main.cpp
+++ main.cpp
@@ -65,7 +65,7 @@ void androidCleanup() {
 int main(int argc, char * argv[])
 {
 
-//	MpxDecoder::saveFilters3("e:\\t\\diff9.wav");
+//	MpxDecoder::saveFilters3("e:\\t\\diff10.wav");
 
 #ifdef COMPILING_ON_ANDROID
 	std::cout.rdbuf(new androidbuf(ANDROID_LOG_INFO, "ReSampler"));
